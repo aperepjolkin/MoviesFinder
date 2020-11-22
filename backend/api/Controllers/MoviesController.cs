d648c9313493977a0a6e457f8e7106e9fa37a4c8
Aleks <abugsbunny@gmail.com>
2020-11-22 15:53:16 +0200

    Init commit

---
+++ backend/api/Controllers/MoviesController.cs
@@ -0,0 +1,39 @@
+﻿using business.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+
+        private readonly ILogger<MoviesController> _logger;
+        private IDataAccess _service;
+
+        public MoviesController(ILogger<MoviesController> logger, IDataAccess service)
+        {
+            _logger = logger;
+            _service = service;
+        }
+
+        [HttpGet]
+        public string Get(string movieTitle)
+        {
+           var movie = _service.FindMovie(movieTitle);
+            // call function from business layer to get data in json format
+            string jsonData = @"{  
+                        'Title':'Die Hard','Year':'1988','Rated':'R','Released':'20 Jul 1988','Runtime':'132 min'
+                        }";
+
+
+            return jsonData;
+        }
+    }
+}
---
+++ backend/business/Data/IDataAccess.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace business.Data
+{
+    public interface IDataAccess
+    {
+        public string FindMovie(string title);
+    }
+}
